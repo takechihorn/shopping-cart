837b615bd45a47bd999a2c4daeef81be5f222282
近藤岳瑠 <takechihorn@gmail.com>
2021-05-21 08:39:48 +0900

    first commit

---
+++ shopping-cart/src/data/items.ts
@@ -0,0 +1,39 @@
+const items = [
+  {
+    id: 1,
+    name: "Comfy Slippers",
+    price: 12.00,
+    category: "Footwear",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+  },
+  {
+    id: 2,
+    name: "Trail Runner",
+    price: 45.50,
+    category: "Footwear",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+  },
+  {
+    id: 3,
+    name: "Hikers",
+    price: 120.50,
+    category: "Footwear",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+  },
+  {
+    id: 4,
+    name: "Ski Jacket",
+    price: 120.50,
+    category: "Jackets",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+  },
+  {
+    id: 5,
+    name: "Winter Parks",
+    price: 230.50,
+    category: "Jackets",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+  }
+]
+
+export default items
---
+++ shopping-cart/src/views/Cart.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>This is the cart Page</div>
+</template>
+
+<script></script>
+
+<style>
+</style>
